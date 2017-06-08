811ba1f15f99aec4d2a8a91d4db09f7bf87515bf
xiaohui.c.liu <xiaohui.c.liu@CDC112093.dir.svc.accenture.com>
2017-06-08 22:36:47 +0800

    初始化提交所有代码，可工作版本

---
+++ zipkinservice/src/main/java/au/gov/myhr/zipkin/Application.java
@@ -0,0 +1,18 @@
+package au.gov.myhr.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+import java.util.logging.Logger;
+
+
+@SpringBootApplication
+@EnableZipkinServer
+public class Application {
+
+	protected Logger logger = Logger.getLogger(Application.class.getName());
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
